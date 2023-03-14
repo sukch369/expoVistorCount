134ca96eb7e29f5953f3b33b576032068e880103
Charles <sukch369@gmail.com>
2023-03-15 00:00:57 +0900

    20230315

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let result_mid = document.getElementById("result_mid"); //중학생
 let result_high = document.getElementById("result_high"); //고등학생
 let result_adult = document.getElementById("result_adult"); //성인
 let result_sum = document.getElementById("result_sum"); //합계
+const inputURL =
+  "https://script.google.com/macros/s/AKfycbwcr95sVJZHktpatokoXEM0jg_n4K1P0wl5aqczOVwjXbzGaGtnisBZkhSaJFZzm9c9/exec";
 
 function plus(button) {
   let result = document.getElementById(button);
@@ -40,4 +42,65 @@ function reset() {
   result_high.value = 0;
   result_adult.value = 0;
   sum();
+}
+
+function confirmMessage() {
+  let message = "".concat(
+    "유치원   (",
+    result_kind.value,
+    "명)\n",
+    "초등학생 (",
+    result_ele.value,
+    "명)\n",
+    "중학생   (",
+    result_mid.value,
+    "명)\n",
+    "고등학생 (",
+    result_high.value,
+    "명)\n",
+    "성인     (",
+    result_adult.value,
+    "명)\n",
+    "총 (",
+    result_sum.value,
+    "명) 이 맞습니까?"
+  );
+  let confirmflag = confirm(message);
+  if (confirmflag) {
+    console.log("OK");
+    sendData();
+    reset();
+  } else {
+    console.log("failed");
+  }
+}
+
+function sendData() {
+  let concatURL = "".concat(
+    inputURL,
+    "?kind=",
+    result_kind.value,
+    "&ele=",
+    result_ele.value,
+    "&mid=",
+    result_mid.value,
+    "&high=",
+    result_high.value,
+    "&adult=",
+    result_adult.value,
+    "&sum=",
+    result_sum.value
+  );
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", concatURL, true);
+  xhr.send();
+  xhr.onload = () => {
+    if (xhr.status == 200) {
+      //success
+      console.log(xhr.response);
+    } else {
+      //failed
+      reset();
+    }
+  };
 }
